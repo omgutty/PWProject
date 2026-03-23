e419eb5a607a028468e8b769c127fd5b00419db5
Om Gutty <om.gutty@gmail.com>
2026-03-23 14:37:44 +0530

    multiple tests

--- tests/secondtest.spec.ts
+++ tests/secondtest.spec.ts
@@ -4,3 +4,13 @@ test('second test', async ({ page})=> {
     await page.goto("https://chatgpt.com/");
 
 })
+
+test('second test2', async ({ page})=> {
+    await page.goto("https://chatgpt.com/");
+
+})
+
+test('second test3', async ({ page})=> {
+    await page.goto("https://chatgpt.com/");
+
+})
