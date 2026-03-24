11430fbcaeac4cc779ebc55c19807638af067454
Om Gutty <om.gutty@gmail.com>
2026-03-24 19:01:33 +0530

    two test part 2

--- tests/Login/Loginstored.spec.ts
+++ tests/Login/Loginstored.spec.ts
@@ -2,18 +2,20 @@ import {test, expect} from '@playwright/test';
 import path from 'node:path';
 
 test("login",async ({page})=>{
+    // await page.context().storageState({path: 'auth/saucedemologin.json'});
     await page.goto('https://www.saucedemo.com/');
     await page.locator('#user-name').fill("standard_user");
     await page.locator('#password').fill("secret_sauce");   
     await page.locator('#login-button').click();
     await page.waitForURL('https://www.saucedemo.com/inventory.html');
     await page.context().storageState({path: 'auth/saucedemologin.json'});
+    
 
 })
 
-test.use({storageState:'auth/saucedemologin.json'})
-test("validate invertory title ",async ({page})=>{
-    await page.goto("https://www.saucedemo.com/inventory.html");
-    // page.locator(".title")
-    await expect(page.locator('.title')).toHaveText("Products");
-})
+// test.use({storageState:'auth/saucedemologin.json'})
+// test("validate invertory title ",async ({page})=>{
+//     await page.goto("https://www.saucedemo.com/inventory.html");
+//     // page.locator(".title")
+//     await expect(page.locator('.title')).toHaveText("Products");
+// })
