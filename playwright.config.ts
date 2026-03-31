3490576d3034000e3ee692457a87e34956660064
Om Gutty <om.gutty@gmail.com>
2026-03-31 14:39:09 +0530

    storage state and parallel exeuction wth workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
     //trace: 'on-first-retry',
     trace: 'on',
     headless: false,
-    
+  //storageState:'loginauth/praticetestautomationlogin.json',
     
   },
 
@@ -53,7 +53,7 @@ export default defineConfig({
       trace: 'on',
       screenshot: 'on',
       //storageState:'auth/saucedemologin.json',
-      //storageState:'loginauth/praticetestautomationlogin.json',
+    
     },
     },
     
