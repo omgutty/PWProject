c0105964f4bb7f02fa207c53f8a27001f4a577f9
Om Gutty <om.gutty@gmail.com>
2026-04-11 09:56:05 +0530

    pagenation program complete

--- tests/table/pagetable.spec.ts
+++ tests/table/pagetable.spec.ts
@@ -4,63 +4,35 @@ test('pagetable',async ({page})=>{
     await page.goto('https://testautomationpractice.blogspot.com/');
 
     const pagenumber= page.locator('.pagination li a');
-
     const countofpages= await pagenumber.count();
+    const paginationtable= page.locator('#productTable');
+    const rows =paginationtable.locator('tbody tr');
+    await expect(rows).toHaveCount(5)
+    const colum= paginationtable.locator('thead th');
+    await expect(colum).toHaveCount(4);
 
     for(let p=0;p<countofpages;p++){
-
         if(p>0){
             await pagenumber.nth(p).click();
             await page.waitForTimeout(2000);
             //how to automatically instead of waiting hardcoded.
-        }
-        //printTableData(page.locator('#productTable'));
-        const rows= page.locator('tbody tr');
-
-        for (let i = 1; i < await rows.count(); i++) {
+        } 
+        for (let i = 0; i < await rows.count(); i++) {
          //give me the current row based on the index
             const eachRow = rows.nth(i);
-             const eachColumn = eachRow.locator('td');
+            const eachColumn = eachRow.locator('td');
             const rowData = [];
-        
+
             //this loop runs inside each row, till the total number of column, 
-        for (let j = 0; j < await eachColumn.count(); j++) {
-            //console.log(await eachcoloum.nth(j).textContent());
-            //get the text from each cell which is present insdie the each column
-            rowData.push(
-                await eachColumn.nth(j).textContent()
+            for (let j = 0; j < await eachColumn.count()-1; j++) {
+                //console.log(await eachcoloum.nth(j).textContent());
+                //get the text from each cell which is present insdie the each column
+                 rowData.push(
+                 await eachColumn.nth(j).textContent()
             );
-        }
-
+            }
+        console.log(rowData);
         } 
-
-
     } 
 })
 
-async function printTableData(table: Locator):Promise<void> {
-     //get the rows locator of the tabel and store in variable  
-    const rows = table.locator('tbody tr');
-
-    //skipping the header as we are starting from 1, 
-    for (let i = 1; i < await rows.count(); i++) {
-         //give me the current row based on the index
-        const eachRow = rows.nth(i);
-        //get all the columns which are having td tag  inside the each row 
-        const eachColumn = eachRow.locator('td');
-
-        //created empty array to for printing perpuse 
-       // const rowData: (string | null)[] = [];
-        const rowData = [];
-        //this loop runs inside each row, till the total number of column, 
-        for (let j = 0; j < await eachColumn.count(); j++) {
-            //console.log(await eachcoloum.nth(j).textContent());
-            //get the text from each cell which is present insdie the each column
-            rowData.push(
-                await eachColumn.nth(j).textContent()
-            );
-        }
-         //print the array with pushed data
-        console.log(rowData);
-    } 
-}
