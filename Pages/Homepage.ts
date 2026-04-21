f6e805a11c174143710b614a135427d9460ff14e
Om Gutty <om.gutty@gmail.com>
2026-04-21 12:15:50 +0530

    page class and page test

--- Pages/Homepage.ts
+++ Pages/Homepage.ts
@@ -1,13 +1,14 @@
-import {Page,Locator}from '@playwright/test'
+import {Page,Locator,expect}from '@playwright/test'
 
-
-class Homepage{
+  class Homepage{
 
     page:Page;
+    homelink:Locator;
     headingText:Locator;
     getstartedbutton:Locator;
-    homelink:Locator;
     searchicon:Locator;
+    searchbar:Locator;
+    searchheader:Locator;
 
     //consturcor is expecting parameter of the page class
     constructor(page:Page){
@@ -19,11 +20,59 @@ class Homepage{
         this.getstartedbutton= page.locator('#get-started');
         this.homelink= page.getByRole('navigation').getByRole('link',{name:'Home'});
         this.searchicon=  page.locator(`//a[@class='zak-header-search__toggle']`).first();
-        //this.searchicon= page.locator(``)
+        this.searchbar= page.locator('input:visible')
+        this.searchheader= page.locator('.zak-page-header__title h1');
         
     }
-}
 
+    //verify text 
+    async gettextvalidate(){//}:Promise<string|null>{
+        //const gettextvalue= await this.headingText.textContent();
+        await expect(this.headingText).toHaveText('Think different. Make different.');
+        //return gettextvalue;
+    }
+
+    //clicking on get start button 
+    async clickonstartbutton():Promise<void>{
+       return  this.getstartedbutton.click()
+    }
+
+    //same as gettext validate but ??''
+    async headingTestvalidate(){
+        return await this.headingText.textContent()??"";
+        //??''  what is the use of this 
+    }
+
+    //click searchicon 
+    async clickingonsearchicon():Promise<void>{
+        await this.searchicon.click();
+    }
+
+    //click on home link
+    async clickonHomeLink():Promise<void>{
+        await this.homelink.click();
+    }
+    
+    //enter product in serch box 
+    async enterproductNameinsearchbox(productname:string){
+         await this.searchbar.fill(productname); 
+         await this.searchbar.press('Enter')
+    }
+
+    //after search verifytitle 
+    // async verifyafterseachResultHeader():Promise<boolean>{
+    //     const searchresultheader=await this.searchheader.textContent();
+    //     console.log(searchresultheader)
+    //     return  searchresultheader?.includes('Search Results') ??"false";
+    // }
+    async verifySearchResultHeader(): Promise<boolean> {
+        const text = await this.searchheader.textContent();
+        //this icludes this value if not send false, direct boolean validation will be done directly
+        return text?.includes('Search Results') ?? false;
+    }
+
+   
+}
 export default Homepage;
 
 //calling the constructor in test like below 
--- tests/POMTest/homepage.spec.ts
+++ tests/POMTest/homepage.spec.ts
@@ -1,20 +1,48 @@
 import{test,expect} from '@playwright/test';
 import Homepage from '../../Pages/Homepage';
+//import TestConfig from '../config.TestConfig'
 
 
 const BaseURL='https://practice.sdetunicorns.com/';;
+let homepage:Homepage;
+
+//create object separetly for each test
+test.beforeEach(async ({page})=>{
+     homepage= new Homepage(page);
+})
+
+//after each test wait for 5 seconds and close the page
+test.afterEach(async ({page})=>{
+    await page.waitForTimeout(5000);
+    await page.close();
+})
 
 test.describe('Home page test',()=>{
 
     test('Open Home page',async ({page})=>{
 
         //creating homepage consructor, so that we will use all the locators which are available in that page
-        const homepage= new Homepage(page);
-
+        
         await page.goto(BaseURL);
+       
+        //validating the heading text
+        expect(await homepage.gettextvalidate()).toBe('Think different. Make different.')
+       
+        //click on start button 
+        await homepage.clickonstartbutton();
 
-        await homepage.getstartedbutton.click();
+        //click on home link 
+        await homepage.clickonHomeLink();
 
+        //click on search icon 
+        await homepage.clickingonsearchicon();
+
+        //enter product name in seach and click enter 
+        await homepage.enterproductNameinsearchbox('The Best Place to Invest Your Money');
+
+        //await homepage.verifyafterseachResultHeader();
+        await homepage.verifySearchResultHeader()
+        
 
     })
 
